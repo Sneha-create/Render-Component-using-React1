00afe2a629b288e912424aee79fe6b023eec4736
sneha shetake <snehashetake.123@gmail.com>
2022-06-30 14:38:29 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import ReactDOM from 'react-dom'
 function App() {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <p>I am learning React. My life is getting better.</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    root.render(element)
+  
   );
 }
 
